f50978b4a6c807d3b376bbb878797c2e839fe0d5
AnDreV133 <dmitrijkubik@gmail.com>
2023-10-25 09:12:28 +0300

    тест

--- src/main/java/org/example/ImagePanel.java
+++ src/main/java/org/example/ImagePanel.java
@@ -1,24 +1,73 @@
 package org.example;
 
-import org.example.MouseController;
-import org.example.PainterLandscape;
-import org.example.PanelPainter;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-
-public class ImagePanel extends JPanel {
-    private PanelPainter panelPainter = new PanelPainter();
+public class ImagePanel extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener {
+    private ImageIcon imageIcon = new ImageIcon(); // изображение
+    private int x, y; // координаты изображения
+    private int xMouse, yMouse; // координаты изображения
+    private double scale = 1.0; // масштабирование
 
     public ImagePanel() {
-        new PainterLandscape(panelPainter, panelPainter.getImageIcon());
-        this.addMouseListener(new MouseController());
+        new PainterLandscape(this, imageIcon);
+
+        x = (Main.WIDTH_IMAGE - imageIcon.getIconWidth()) / 2; // начальные координаты изображения по центру панели
+        y = (Main.HEIGHT - imageIcon.getIconHeight()) / 2;
+        addMouseListener(this); // добавляем слушателей событий мыши
+        addMouseMotionListener(this);
+        addMouseWheelListener(this);
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) { // переопределяем метод отрисовки компонента
         super.paintComponent(g);
-        panelPainter.updateImage(g);
+        Graphics2D g2d = (Graphics2D) g; // используем 2D графику
+        g2d.scale(scale, scale); // масштабируем изображение
+        g2d.drawImage(imageIcon.getImage(), (int) (x / scale), (int) (y / scale), this); // отрисовываем изображение
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    } // оставляем пустыми, так как не используем эти события
+
+    public void mousePressed(MouseEvent e) { // запоминаем координаты мыши при нажатии
+        xMouse = e.getX();
+        yMouse = e.getY();
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    } // оставляем пустыми
+
+    public void mouseEntered(MouseEvent e) {
+    } // оставляем пустыми
+
+    public void mouseExited(MouseEvent e) {
+    } // оставляем пустыми
+
+    public void mouseDragged(MouseEvent e) { // перемещаем изображение при перетаскивании мыши
+        int dx = e.getX() - xMouse; // смещение по x
+        int dy = e.getY() - yMouse; // смещение по y
+        x += dx;
+        y += dy;
+        repaint();// перерисовываем компонент
+        xMouse = e.getX();
+        yMouse = e.getY();
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    } // оставляем пустыми
+
+    public void mouseWheelMoved(MouseWheelEvent e) { // масштабируем изображение при вращении колесика мыши
+        int notches = e.getWheelRotation();
+        if (notches < 0) {
+            scale *= 1.1; // увеличиваем масштаб на 10%
+        } else {
+            scale /= 1.1; // уменьшаем масштаб на 10%
+        }
+
+        if (scale < 0.1)
+            scale = 0.1;
+
+        repaint(); // перерисовываем компонент
     }
 }
